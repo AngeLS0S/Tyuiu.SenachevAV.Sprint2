0b6d80b51a6600ca9120fd30fdd53d8a559635fa
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-10-18 19:45:51 +0500

    Task1

---
+++ Tyuiu.SenachevAV.Sprint2.Task1.V6/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.SenachevAV.Sprint2.Task1.V6;
+using Tyuiu.SenachevAV.Sprint2.Task1.V6.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int a = 915, b = 169, c = 174, d = 133;
+
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+
+        Console.Title = "Спринт #2 | Выполнил: Сеначев А.В. | АСОиУб-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович | АСОиУб-24-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,)        *");
+        Console.WriteLine("*последовательность можно чередовать, но использовать один раз в выражении*");
+        Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^, последовательность           *");
+        Console.WriteLine("* операций не должна нарушаться)                                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("A = " + a);
+        Console.WriteLine("B = " + b);
+        Console.WriteLine("C = " + c);
+        Console.WriteLine("D = " + d);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
